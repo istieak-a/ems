1af4452b35d0de34518e869bc074b3cf626f951f
istieak-a <istieak.ahmed@yahoo.com>
2024-12-02 16:18:08 +0600

    Refactor authentication and dashboard rendering logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
 import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
-
+import { AuthContext } from "./context/AuthProvider";
 const App = () => {
   const [user, setUser] = useState(null);
 
@@ -18,10 +18,13 @@ const App = () => {
       alert("Invalid credentials");
     }
   }
-
+  const data = useContext(AuthContext);
+  console.log(data); 
   return (
     <>
       {!user ? <Login handleLogin={handleLogin} /> : ''}
+      {user == 'admin' ? <AdminDashboard /> : ''}
+      {user == 'user' ? <EmployeeDashboard /> : ''}
     </>
   );
 };
---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,26 @@
+import React, { createContext, useEffect, useState } from 'react'
+import { getLocalStorage } from '../utils/localStorage';
+
+export const AuthContext = createContext(); 
+
+const AuthProvider = ({children}) => {
+
+    const [userData, setUserData] = useState([]);
+
+    useEffect(() => {
+        const {employees, admin} = getLocalStorage();
+        setUserData({admin,employees});
+    }, []); 
+    // console.log(userData);
+    
+
+  return (
+    <div>
+        <AuthContext.Provider value={userData}>
+            {children}
+        </AuthContext.Provider>
+    </div>
+  )
+}
+
+export default AuthProvider
